caa3d5c8177c3867cee3a2ad8b124b362e5e3713
SteamMachina <52200491+SteamMachina@users.noreply.github.com>
2021-02-08 14:16:20 +0100

    Update exercice1.py

--- exercices/exercice1.py
+++ exercices/exercice1.py
@@ -1,2 +1,9 @@
-def maxi():
-
+def maxi(T):
+  max = T[0]
+  id = 0
+  for i in range(len(T)):
+    if max < T[i]:
+      max = T[i]
+      id = i
+  return (max, id)
+assert maxi([1,5,6,9,1,2,3,7,9,8]) == (9,3)
